e13d20111521e0725c10e9c90bdebab3e44ee564
Abhishek Kulkarni <abhishekkulkarniak8@gmail.com>
2026-02-19 13:35:26 +0530

    basic routing and components and daisyui and tailwind configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
-import './App.css'
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Body from './components/Body';
+import Login from './components/Login';
+import Profile from './components/Profile';
 
 function App() {
-  return (
-    
-      <div>
-        Hello World
-       </div>
+  
+return (
+  <>
+  <BrowserRouter basename='/'>
+  <Routes>
+    <Route path='/' element={<Body/>}>
+    <Route path='/login' element={<Login/>}/>
+    <Route path='/profile' element={<Profile/>}/>
+    </Route>
+  </Routes>
+ 
+  </BrowserRouter>
+   </>
+)
+   
 
-  )
+  
 }
 
 export default App
